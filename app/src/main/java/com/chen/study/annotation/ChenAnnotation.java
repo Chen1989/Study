2d8432dff0049c3e9b74c7306c69ea6c1f6cf1a0
peng.chen <peng.chen@joyreach.com>
2017-09-12 21:58:06 +0800

    **增加注解，待完善**

---
+++ app/src/main/java/com/chen/study/annotation/ChenAnnotation.java
@@ -0,0 +1,13 @@
+package com.chen.study.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by PengChen on 2017/9/12.
+ */
+
+@Retention(RetentionPolicy.CLASS)
+public @interface ChenAnnotation {
+
+}
